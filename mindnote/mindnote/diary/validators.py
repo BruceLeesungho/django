e76fc7b15bf46b293e945f075ca96a5d23b66371
BruceLeesungho <87701815+BruceLeesungho@users.noreply.github.com>
2022-02-14 22:49:10 +0900

    add delete , update
    almost complete

--- mindnote/mindnote/diary/validators.py
+++ mindnote/mindnote/diary/validators.py
@@ -1,16 +1,17 @@
-# 이곳에 작성합니다.
 from django.core.exceptions import ValidationError
 
 
 def validate_no_hash(value):
-    if "#" in value:
-        raise ValidationError("#이 들어갈 수 없습니다.")
-        
+    if '#' in value:
+        raise ValidationError('# 은 포함될 수 없습니다.')
+
+
 def validate_no_numbers(value):
     for ch in value:
         if ch.isdigit():
-            raise ValidationError("숫자가 들어갈 수 없습니다.")
-    
+            raise ValidationError('숫자는 들어갈 수 없습니다.')
+
+
 def validate_score(value):
-    if value > 10 or value < 0:
-        raise ValidationError("0부터 10사이의 숫자만 들어갈 수 있습니다.")
+    if 0 > value or 10 < value:
+        raise ValidationError('0부터 10사이의 숫자만 입력 가능합니다.')
--- mindnote/mindnote/diary/views.py
+++ mindnote/mindnote/diary/views.py
@@ -4,6 +4,10 @@ from .forms import PageForm
 
 
 # Create your views here.
+
+# 이곳에 index 뷰를 작성해 주세요.
+
+
 def page_list(request):
     object_list = Page.objects.all()
     return render(request, 'diary/page_list.html', {'object_list': object_list})
@@ -18,7 +22,7 @@ def page_create(request):
         form = PageForm(request.POST)
         if form.is_valid():
             new_page = form.save()
-            return redirect('page-detail', page_id = new_page.id)
+            return redirect('page-detail', page_id=new_page.id)
     else:
         form = PageForm()
     return render(request, 'diary/page_form.html', {'form': form})
@@ -27,3 +31,27 @@ def page_create(request):
 def page_detail(request, page_id):
     object = Page.objects.get(id=page_id)
     return render(request, 'diary/page_detail.html', {'object': object})
+
+
+def page_update(request, page_id):
+    object = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        form = PageForm(request.POST, instance=object)
+        if form.is_valid():
+            form.save()
+            return redirect('page-detail', page_id=object.id)
+    else:
+        form = PageForm(instance=object)
+    return render(request, 'diary/page_form.html', {'form': form})
+
+
+def page_delete(request, page_id):
+    object = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        object.delete()
+        return redirect('page-list')
+    else:
+        return render(request, 'diary/page_confirm_delete.html', {'object': object})
+        
+def index(request):
+    return render(request, 'diary/index.html')
